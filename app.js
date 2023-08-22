4d6f891903e36a97a4dd183f06fcdc0731e1150e
23mervinwijaya <23mervinwijaya@gmail.com>
2023-08-22 11:36:55 +0700

    routing

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require ('http');
+const port = 3000;
+const fs = require('fs');
+
+
+// RenderFile
+const renderHTML=(req,res)=>{
+    const url = req.url;
+    console.log(url);
+
+    const method = req.method;
+    console.log(method);
+    
+    fs.readFile(`./${url}.html`,(err,data)=>{
+        if(err){
+            res.writeHead(404);
+            res.write(fs.readFileSync('./404.html'))
+        }
+        else{
+            res.write(data)
+        }
+        res.end();
+    })
+}
+
+
+// Create Server
+http
+.createServer(renderHTML)
+.listen(port,()=>{
+    console.log(`Server is listening to port ${port}`)
+})
